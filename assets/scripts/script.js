1fd2d2bd1825d02b634de31c0ab3e5396f9cd545
Whefert <jeffersondaley@gmail.com>
2023-12-28 14:35:12 +0000

    Get current weather and date for header

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,25 +2,23 @@ const openWeatherAPIKey = "4974ac4d803cd7f639c43cfd1f563096";
 let cityLatitude, cityLongitude;
 
 //on click of search button,
-$("form").on("submit", function (event) {
+$(".search-button").on("click", function (event) {
   //stop default behaviour of form
   event.preventDefault();
   //Get the city name for search
   let city = $("#search-input").val();
 
   //take search input value, add it to local storage
-  localStorage.setItem(`searchItem_${localStorage.length + 1}`, city);
-  showPreviousSearches();
+
   if (!city) {
     alert("Search cannot be empty. Please input the name of a city");
     return;
   }
-
+  localStorage.setItem(`searchItem_${localStorage.length + 1}`, city);
+  showPreviousSearches();
   fetchCityWeatherForecast(city);
 });
-// fetch(
-//   `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-// );
+
 async function fetchCityLatAndLong(cityName) {
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${openWeatherAPIKey}`
@@ -34,24 +32,47 @@ async function fetchCityWeatherForecast(cityName) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${openWeatherAPIKey}`
   );
+
   const weatherForecast = await response.json();
-  console.dir(weatherForecast);
-  createForecastItems(weatherForecast.list);
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${openWeatherAPIKey}`
+  );
+
+  let currentWeather = await res.json();
+  console.dir(currentWeather);
+
+  let currentWeatherObj = {
+    name: currentWeather.name,
+    temp: currentWeather.main.temp,
+    wind: currentWeather.wind.speed,
+    humidity: currentWeather.main.humidity,
+    weather: currentWeather.weather.description,
+  };
+  updateCurrentWeather(currentWeatherObj);
+  // createForecastItems(weatherForecast.list);
+}
+
+function updateCurrentWeather(currentWeather) {
+  $("#today .city").text(currentWeather.name);
+  $("#today .date").text(`(${dayjs().format("DD/MM/YYYY")})`);
+  // $("span.clouds").text();
+  $("#today .temp").text(currentWeather.temp);
+  $("#today .wind").text(currentWeather.wind);
+  $("#today .humidity").text(currentWeather.humidity);
 }
 
 function createForecastItems(weatherData) {
+  $("#fiveDayForecast").empty();
   for (let i = 1; i < 6; i++) {
-    const row = $("<div>");
-    row.addClass("row");
-    row.attr("id", `row_${i}`);
+    const row = $(`<div class='row gx-2 my-2'  id='row_${i}'>`);
     $("#fiveDayForecast").append(row);
   }
 
-  for (let i = 1; i < weatherData.length; i++) {
+  for (let i = 0; i < weatherData.length; i++) {
     const weather = weatherData[i];
     //create forecast element
     const div = $("<div>");
-    div.addClass("col flex-column forecastDay text-white pt-3");
+    div.addClass("col mx-2 flex-column forecastDay text-white pt-3");
     date = $("<p>");
     date.text(dayjs.unix(weather.dt).format("DD/MM/YYYY"));
     div.append(date);
@@ -72,8 +93,11 @@ function createForecastItems(weatherData) {
 
 function showPreviousSearches() {
   for (let i = 1; i < localStorage.length + 1; i++) {
-    let historyItem = $("<button class='btn btn-secondary mb-2'>");
-    historyItem.append(localStorage.getItem(`searchItem_${i}`));
+    let city = localStorage.getItem(`searchItem_${i}`);
+    let historyItem = $(
+      `<button class='btn btn-secondary mb-2 history-item' data-city='${city}'>`
+    );
+    historyItem.append(city);
     $("#history").append(historyItem);
   }
 }
@@ -83,4 +107,17 @@ function init() {
   showPreviousSearches();
 }
 
+function clearHistory() {
+  localStorage.clear();
+  showPreviousSearches();
+}
+
+$("#clear-search-button").on("click", () => {
+  clearHistory();
+});
+
 init();
+$(".history-item").on("click", function (event) {
+  city = event.target.dataset.city;
+  fetchCityWeatherForecast(city);
+});
